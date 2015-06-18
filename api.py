333caae7b0ea64afbc77285d9223b988d618c17d
Myles Braithwaite <me@mylesbraithwaite.com>
2015-06-18 12:13:56 -0400

    Fixing PEP8.

--- api.py
+++ api.py
@@ -20,25 +20,25 @@ cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 @app.route('/')
 def index():
     data = {}
-    
+
     with open(os.path.join(app.data_dir, 'me.json'), 'r') as f:
         data['me'] = json.loads(f.read())
-    
+
     with open(os.path.join(app.data_dir, 'modules.json'), 'r') as f:
         settings = json.loads(f.read())
-    
+
     modules = settings.get('modules', None)
-    
+
     data['routes'] = []
-    
+
     for m in modules:
-        data['routes'] += ["/" + m.get('path'),]
-    
+        data['routes'] += ["/" + m.get('path'), ]
+
     data['routes'].sort()
-    
+
     res = jsonify(data)
     res.headers['Access-Control-Allow-Origin'] = '*'
-    
+
     return res
 
 
@@ -48,27 +48,27 @@ def send_text_file():
 
 
 @app.route('/<string:path>')
-@cache.cached(timeout=60*60*2) # 2 hours
+@cache.cached(timeout=60 * 60 * 2)  # 2 hours
 def module(path):
     with open(os.path.join(app.data_dir, 'modules.json'), 'r') as f:
         modules = json.loads(f.read())['modules']
-    
+
     module = None
-    
+
     for i, dic in enumerate(modules):
         if dic['path'] == path:
             module = modules[i]
-    
+
     if not module:
         abort(404)
-    
+
     middleware = importlib.import_module("middleware.module_" + module['type'])
-    
+
     data = middleware.main(app, module.get('data', {}))
-    
+
     res = jsonify(data)
     res.headers['Access-Control-Allow-Origin'] = '*'
-    
+
     return res
 
 
@@ -76,7 +76,7 @@ def module(path):
 def page_not_found(error):
     res = jsonify(status=404, error='not_found')
     res.headers['Access-Control-Allow-Origin'] = '*'
-    
+
     return res, 404
 
 
