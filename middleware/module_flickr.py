59b740ca7f9222102e71a2c6a289d5f3e2308461
Myles Braithwaite <me@mylesbraithwaite.com>
2015-06-18 11:59:46 -0400

    Added a bunch of extra stuff.

--- middleware/module_flickr.py
+++ middleware/module_flickr.py
@@ -9,7 +9,8 @@ def main(app, data):
     url_data = {
         'method': 'flickr.people.getPhotos',
         'api_key': environ.get('FLICKR_KEY'),
-        'user_id': data.get('nsid')
+        'user_id': data.get('nsid'),
+        'extras': 'description, license, date_upload, date_taken, owner_name, icon_server, original_format, last_update, geo, tags, machine_tags, o_dims, views, media, path_alias, url_sq, url_t, url_s, url_q, url_m, url_n, url_z, url_c, url_l, url_o'
     }
     
     r = requests.post('https://api.flickr.com/services/rest/', data=url_data)
