c0de2a081cfe9af7f6b9d39daae557d45f5d69ee
Myles Braithwaite <me@mylesbraithwaite.com>
2015-10-21 12:05:43 -0400

    Allow remote and local files.

--- middleware/module_yaml.py
+++ middleware/module_yaml.py
@@ -3,12 +3,29 @@ from __future__ import unicode_literals
 import os
 
 import yaml
+import requests
 
 
-def main(app, data):
+def local(app, data):
     filepath = os.path.join(app.data_dir, data.get('filename'))
 
     with open(filepath, 'r') as f:
         contents = yaml.load(f)
 
     return contents
+
+
+def remote(app, data):
+    r = requests.get(data.get('url'))
+
+    contents = yaml.load(r.data)
+
+    return contents
+
+
+def main(app, data):
+    if data.get('filename'):
+        return local(app, data)
+
+    if data.get('url'):
+        return remote(app, data)
