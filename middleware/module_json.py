86d4bce9b07b4bda15d2147b7ae7c9a10e1f83ce
Myles Braithwaite <me@mylesbraithwaite.com>
2015-10-21 12:06:00 -0400

    Added support for local files with json.

--- middleware/module_json.py
+++ middleware/module_json.py
@@ -1,9 +1,29 @@
 from __future__ import unicode_literals
 
+import os
+
+import json
 import requests
 
 
-def main(app, data):
+def local(app, data):
+    filepath = os.path.join(app.data_dir, data.get('filename'))
+
+    with open(filepath, 'r') as f:
+        contents = json.loads(f)
+
+    return contents
+
+
+def remote(app, data):
     r = requests.get(data.get('api_url'))
 
     return r.json()
+
+
+def main(app, data):
+    if data.get('filename'):
+        return local(app, data)
+
+    if data.get('url'):
+        return remote(app, data)
