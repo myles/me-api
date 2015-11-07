19873b5300f05c6bfa32b39ec7d62d328c7364ef
Myles Braithwaite <me@mylesbraithwaite.com>
2015-11-07 11:52:16 -0500

    At #PyConCA2015 tutorial on ripozo.

--- api.py
+++ api.py
@@ -1,15 +1,21 @@
+#!/usr/bin/python
+
 import os
 import importlib
-
-import yaml
 
 from flask import Flask, json, jsonify, abort, request
 
 from flask.ext.cache import Cache
+from flask_ripozo import FlaskDispatcher
 
-from utils import CustomJSONEncoder, yamlify
+from ripozo import apimethod, ResourceBase
+
+from utils import CustomJSONEncoder, JSONRipozoAdapter
 
 app = Flask(__name__, instance_relative_config=True)
+
+dispatcher = FlaskDispatcher(app, url_prefix='/v1')
+dispatcher.register_adapters(JSONRipozoAdapter)
 
 app.root_dir = os.path.dirname(os.path.abspath(__file__))
 app.data_dir = os.path.join(app.root_dir, 'data')
@@ -20,66 +26,56 @@ app.static_url_path = os.path.join(app.root_dir, 'templates')
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 
-@app.route('/')
-@cache.cached(timeout=60 * 60 * 1)  # 1 hours
-def index():
-    data = {}
+class IndexViewSet(ResourceBase):
+    resource_name = ''
 
-    with open(os.path.join(app.data_dir, 'me.json'), 'r') as f:
-        data['me'] = json.loads(f.read())
+    @apimethod(methods=['GET'])
+    def index(cls, request, *args, **kwargs):
+        data = {}
 
-    with open(os.path.join(app.data_dir, 'config.json'), 'r') as f:
-        config = json.loads(f.read())
+        with open(os.path.join(app.data_dir, 'me.json'), 'r') as f:
+            data['me'] = json.loads(f.read())
 
-    data['routes'] = []
+        with open(os.path.join(app.data_dir, 'config.json'), 'r') as f:
+            config = json.loads(f.read())
 
-    for route in config.get('modules').keys():
-        data['routes'] += [route, ]
+        data['routes'] = []
 
-    data['routes'].sort()
+        for route in config.get('modules').keys():
+            data['routes'] += [dispatcher.url_prefix + route, ]
 
-    if request.args.get('format') == 'yaml':
-        res = yamlify(data)
-    else:
-        res = jsonify(data)
+        data['routes'].sort()
 
-    res.headers['Access-Control-Allow-Origin'] = '*'
+        return cls(properties=data)
 
-    return res
+
+class ModuleViewSet(ResourceBase):
+    resource_name = ''
+    pks = ('module_name',)
+
+    @apimethod(methods=['GET'])
+    def view(cls, request, *args, **kwargs):
+        with open(os.path.join(app.data_dir, 'config.json'), 'r') as f:
+            config = json.loads(f.read())
+
+        module_name = request.get('module_name')
+
+        module_config = config.get('modules').get("/" + module_name)
+
+        try:
+            middleware = importlib.import_module("middleware.module_" +
+                                                 module_config.get('module'))
+        except ImportError:
+            abort(404)
+
+        data = middleware.main(app, module_config.get('data', {}))
+
+        return cls(properties=data)
 
 
 @app.route('/robots.txt')
 def send_text_file():
     return app.send_static_file(request.path[1:])
-
-
-@app.route('/<path:path>')
-@cache.cached(timeout=60 * 60 * 2)  # 2 hours
-def module(path):
-    with open(os.path.join(app.data_dir, 'config.json'), 'r') as f:
-        config = json.loads(f.read())
-
-    module = config.get('modules').get("/%s" % path)
-
-    if not module:
-        abort(404)
-
-    try:
-        middleware = importlib.import_module("middleware.module_" +
-                                             module.get('module'))
-    except ImportError:
-        abort(404)
-
-    data = middleware.main(app, module.get('data', {}))
-
-    if request.args.get('format') == 'yaml':
-        res = yamlify(data)
-    else:
-        res = jsonify(data)
-
-    res.headers['Access-Control-Allow-Origin'] = '*'
-
-    return res
 
 
 @app.errorhandler(404)
@@ -89,6 +85,7 @@ def page_not_found(error):
 
     return res, 404
 
+dispatcher.register_resources(IndexViewSet, ModuleViewSet)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- utils.py
+++ utils.py
@@ -1,20 +1,27 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import time
 import datetime
+
+import six
 
 try:
     from urlparse import urlparse, urlunparse
 except ImportError:
     from urllib.parse import urlparse, urlunparse
 
-import yaml
-
-from flask.json import JSONEncoder
+from flask import json
 from flask.globals import current_app, request
+
+from ripozo.adapters import AdapterBase
 
 from instagram import models as instagram_models
 
 
-class CustomJSONEncoder(JSONEncoder):
+class CustomJSONEncoder(json.JSONEncoder):
 
     def default(self, obj):
 
@@ -46,11 +53,97 @@ def remove_utm(url):
     return utmless_url
 
 
-def yamlify(*args, **kwargs):
-    rv = current_app.response_class(
-            yaml.safe_dump(
-                dict(*args, **kwargs), line_break='\n',
-                default_flow_style=False),
-            mimetype="application/x-yaml")
+class JSONRipozoAdapter(AdapterBase):
+    """
+    Just a plain old JSON dump of the properties.
+    Nothing exciting.
 
-    return rv
+    Format:
+
+    .. code-block:: javascript
+
+        <resource_name>: {
+            field1: "value"
+            field2: "value"
+            relationship: {
+                relationship_field: "value"
+            }
+            list_relationship: [
+                {
+                    relationship_field: "value"
+                }
+                {
+                    relationship_field: "value"
+                }
+            ]
+        }
+    """
+    _CONTENT_TYPE = 'application/json'
+    formats = ['json', _CONTENT_TYPE]
+    extra_headers = {'Content-Type': _CONTENT_TYPE}
+
+    @property
+    def formatted_body(self):
+        """
+        :return: The formatted body that should be returned.
+            It's just a ``json.dumps`` of the properties and
+            relationships
+        :rtype: unicode
+        """
+        response = dict()
+        parent_properties = self.resource.properties.copy()
+        self._append_relationships_to_list(response,
+                                           self.resource.related_resources)
+        self._append_relationships_to_list(response,
+                                           self.resource.linked_resources)
+        response.update(parent_properties)
+        return json.dumps({self.resource.resource_name: response})
+
+    @staticmethod
+    def _append_relationships_to_list(rel_dict, relationships):
+        """
+        Dumps the relationship resources provided into
+        a json ready list of dictionaries.  Side effect
+        of updating the dictionary with the relationships
+
+        :param dict rel_dict:
+        :param list relationships:
+        :return: A list of the resources in dictionary format.
+        :rtype: list
+        """
+        for resource, name, embedded in relationships:
+            if name not in rel_dict:
+                rel_dict[name] = []
+            if isinstance(resource, (list, tuple)):
+                for res in resource:
+                    rel_dict[name].append(res.properties)
+                continue
+            rel_dict[name].append(resource.properties)
+
+    @classmethod
+    def format_exception(cls, exc):
+        """
+        Takes an exception and appropriately formats
+        the response.  By default it just returns a json dump
+        of the status code and the exception message.
+        Any exception that does not have a status_code attribute
+        will have a status_code of 500.
+
+        :param Exception exc: The exception to format.
+        :return: A tuple containing: response body, format,
+            http response code
+        :rtype: tuple
+        """
+        status_code = getattr(exc, 'status_code', 500)
+        body = json.dumps(dict(status=status_code, message=six.text_type(exc)))
+        return body, cls.formats[0], status_code
+
+    @classmethod
+    def format_request(cls, request):
+        """
+        Simply returns request
+
+        :param RequestContainer request: The request to handler
+        :rtype: RequestContainer
+        """
+        return request
